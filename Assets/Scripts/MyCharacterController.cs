e76c60b24bd143d0ec6551819fff57cd85e780bd
Jens Deweirdt <jens31@live.be>
2018-09-25 17:05:49 +0200

    Basic movement Player

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour
+{
+    public float jumpForce = 400f;                          // Amount of force added when the player jumps.
+    public float crouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
+    public float movementSmoothing = .05f;  // How much to smooth out the movement
+    public bool airControl = false;                         // Whether or not a player can steer while jumping;
+
+    bool isGrounded;            // Whether or not the player is grounded.
+    Rigidbody2D m_Rigidbody2D;
+    bool facingRight = true;  // For determining which way the player is currently facing.
+    Vector3 velocity = Vector3.zero;
+
+
+    bool m_wasCrouching = false;
+
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = true;
+    }
+
+
+    public void Move(float move, bool isCrouching, bool isJumping)
+    {
+        //only control the player if grounded or airControl is turned on
+        if (isGrounded || airControl)
+        {
+
+            // If crouching
+            if (isCrouching)
+            {
+                if (!m_wasCrouching)
+                {
+                    m_wasCrouching = true;
+                }
+
+                // Reduce the speed by the crouchSpeed multiplier
+                move *= crouchSpeed;
+            }
+            else
+            {
+
+                if (m_wasCrouching)
+                {
+                    m_wasCrouching = false;
+                }
+            }
+
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            // And then smoothing it out and applying it to the character
+            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !facingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            //else if (move < 0 && m_FacingRight)
+            //{
+            //    // ... flip the player.
+            //    Flip();
+            //}
+        }
+        // If the player should jump...
+        if (isGrounded && isJumping)
+        {
+            // Add a vertical force to the player.
+            isGrounded = false;
+            m_Rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public MyCharacterController controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    Animation anim;
+
+    bool isAttacking = false;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        CheckPlayerInputAndActAccordingly();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isAttacking)
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+
+    private void CheckPlayerInputAndActAccordingly()
+    {
+        if (Mathf.Abs(horizontalMove) > 0.01f && !isAttacking)
+            anim.Play("Rogue_walk_01");
+        if (Mathf.Abs(horizontalMove) == 0f && !isAttacking)
+            anim.Play("Rogue_idle_01");
+        if (Input.GetButtonDown("Fire1") && !isAttacking)
+        {
+            StartCoroutine(AttackZeroOne());
+        }
+        if (Input.GetButtonDown("Fire2") && !isAttacking)
+        {
+            StartCoroutine(AttackZeroThree());
+        }
+    }
+
+    IEnumerator AttackZeroOne()
+    {
+        isAttacking = true;
+        anim.Play("Rogue_attack_01");
+        yield return new WaitForSeconds(1.5f);
+        isAttacking = false;
+    }
+
+    IEnumerator AttackZeroThree()
+    {
+        isAttacking = true;
+        anim.Play("Rogue_attack_03");
+        yield return new WaitForSeconds(0.4f);
+        isAttacking = false;
+    }
+}
